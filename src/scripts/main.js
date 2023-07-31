87ad892d7ef70b732626afe3734e01df3d6aabee
Eduardo Vieira <carlosevhs@gmail.com>
2023-07-31 15:14:01 -0300

    adicinando secao de elenco e rodapé

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,25 +14,8 @@ document.addEventListener('DOMContentLoaded', function(){
             exibeElementosDoHeader();
         }
     })
-
-    //seção de atrações, programação das abas
-    for (let i = 0; i< buttons.length; i++){
-        buttons[i].addEventListener('click', function(botao){
-            const abaAlvo = botao.target.dataset.tabButton
-            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
-            escondeTodasAbas();
-            aba.classList.add('shows__list--is-active');
-            removeBotaoAtivo()
-            botao.target.classList.add('shows__tabs__button--is-active');
-        })
-    }
-
-
-    //seção FAQ, acordeons
-    for (let i = 0;i<questions.length; i++){
-        questions[i].addEventListener('click',abraOuFechaResposta);
-    }
 })
+
 
 
 function ocultaElementosDoHeader(){
@@ -43,27 +26,4 @@ function ocultaElementosDoHeader(){
 function exibeElementosDoHeader(){
     const header = document.querySelector('header');
     header.classList.remove('header--is-hidden');
-}
-
-function abraOuFechaResposta(elemento){
-    const classe = 'faq__questions__item--is-open';
-    const elementoPai = elemento.target.parentNode;
-
-    elementoPai.classList.toggle(classe);
-}
-
-function removeBotaoAtivo() {
-    const buttons = document.querySelectorAll('[data-tab-button]');
-    
-    for (let i = 0; i< buttons.length; i++){
-        buttons[i].classList.remove('shows__tabs__button--is-active');
-    }
-}
-
-function escondeTodasAbas() {
-    const tabsContainer = document.querySelectorAll('[data-tab-id]');
-
-    for (let i=0; i<tabsContainer.length; i++) {
-        tabsContainer[i].classList.remove('shows__list--is-active');
-    }
 }
